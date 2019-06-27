8455deccef168b217b3b837992d04338d6d54c1e
Danilo <danilorb20099@hotmail.com>
2019-06-27 13:48:17 -0600

    proyecto con validaciones y resultados esperados

--- js/main.js
+++ js/main.js
@@ -1,12 +1,16 @@
 document.querySelector('form').addEventListener('submit',function(e){
 
-    validation();
+    if(validation()){
     var salary=document.getElementById("Salary").value;
     var years=document.getElementById("Years").value;
     var days=document.getElementById("Days").value;
     calculate(salary,years,days);
       e.preventDefault();
-    });
+   
+
+}
+});
+
 
 function validation(){
 
@@ -16,9 +20,11 @@ function validation(){
 	  }else if(document.getElementById("Years").value== "" ){
 		alert('Please Complete years input with a number less than 35');
 		return false;
-      }else if(document.getElementById("Days").value=="" || document.getElementById("Days").value==0|| document.getElementById("Days").value>=365){
+      }else if(document.getElementById("Days").value=="" || document.getElementById("Days").value==0|| document.getElementById("Days").value>365){
 		alert('Please Complete Days  input with a number less than 365');
 		return false;
+      }else{
+          return true;
       }
       
 }
@@ -27,34 +33,36 @@ function calculate(salary,years,days){
 
     var result;
     var salary15;
-if(years<=0){
-    salary15=(salary*15)/30;
     
-    result=(salary15*days)/365;
-    showresult(result);
-}
-else if(years>=1 && years<=3){
-    result=(salary*15)/30;
+    if(years<=0){
+        salary15=(salary*15)/30;
+        result=parseFloat(Math.round(((salary15*days)/365) * 100) / 100).toFixed(2);
+        showresult(result);
+    }
+    else if(years>=1 && years<=3){
+        salary15=Math.round((salary*15)/30);
+        result=parseFloat(Math.round(((salary15*days)/365) * 100) / 100).toFixed(2);
     
-   // result=(salary15*days)/365;
-    showresult(result);
+        showresult(result);
 
-showresult(result);
-}
-else if(years>=3 && years<=10){
-    result=(salary*19)/30;
-    
-    //result=(salary15*days)/365;
     showresult(result);
-}else{
-    result=(salary*21)/30;
-   
-    //result=(salary15*days)/365;
-    showresult(result);
-}
+    }
+    else if(years>=3 && years<=10){
+        salary15=Math.round((salary*19)/30);
+        result=parseFloat(Math.round(((salary15*days)/365) * 100) / 100).toFixed(2);
+        
+        showresult(result);
+    }else{
+        salary15=Math.round((salary*21)/30);
+        result=parseFloat(Math.round(((salary15*days)/365) * 100) / 100).toFixed(2);
+        
+        showresult(result);
+    }
 }
 
 function showresult(result){
 
     Swal.fire('Your Bonus is $'+result);
 }
+
+
